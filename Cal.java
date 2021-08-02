5e319b7a18c1a099ab2b0831d7cfe20f77b77286
ashprac <ashwini.practice1@gmail.com>
2021-08-02 15:54:15 +0000

    m3 third commit

--- Cal.java
+++ Cal.java
@@ -5,7 +5,14 @@ class Cal
              int a=10; 
 	     int b=20;
 	     int c=a+b;
+	     int d=a-b;
+	     int e=a*b;
+	     int f=a/b;
 	     System.out.println(c);
+	     System.out.println(d);
+             System.out.println(e);
+             System.out.println(f);
+ 
 	}
 }
 
