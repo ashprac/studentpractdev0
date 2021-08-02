337fc0e410838a066a79595229e8c568cfafd551
ashprac <ashwini.practice1@gmail.com>
2021-08-02 15:57:07 +0000

    f1 feature first commit

--- Cal.java
+++ Cal.java
@@ -8,10 +8,13 @@ class Cal
 	     int d=a-b;
 	     int e=a*b;
 	     int f=a/b;
+	     int g=a%b;
 	     System.out.println(c);
 	     System.out.println(d);
              System.out.println(e);
              System.out.println(f);
+	     System.out.println(g);
+
  
 	}
 }
